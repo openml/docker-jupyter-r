091c3af62d997e081c0b59abd26579722127548a
Andrey Ustyuzhanin <andrey.u@gmail.com>
2016-09-06 18:41:42 +0200

    libpath

--- install.r
+++ install.r
@@ -1,6 +1,6 @@
 library(devtools)
 
 install_github("IRkernel/repr")
-install_github("mlr-org/mlr", local=FALSE)
-install_github("mlr-org/farff", local=FALSE)
-install_github("openml/r", local=FALSE)
+install_github("mlr-org/mlr", local=FALSE, lib="/opt/conda/lib/R/library")
+install_github("mlr-org/farff", local=FALSE, lib="/opt/conda/lib/R/library")
+install_github("openml/r", local=FALSE, lib="/opt/conda/lib/R/library")
